a6f013ba6c34cbd4937487367520f9cd065994db
Stackbit Code Editor <projects@stackbit.com>
2021-04-16 20:02:00 +0000

    service.js: changed

--- src/components/service.js
+++ src/components/service.js
@@ -7,8 +7,7 @@ const Service = ({ service }) => (
         {/* <div className="service__image-wrap">
             <Img fluid={ service.image.fluid } alt="service item" className="service__image"/>
         </div> */}
-                    <i className="item__icon material-icons text-primary">{ feature.icon }</i>
-
+        <i className="item__icon material-icons text-primary">{ feature.icon }</i>
         <h3 className="service__title">{ service.title }</h3>
         <p>{ service.description.description }</p>
     </div>
