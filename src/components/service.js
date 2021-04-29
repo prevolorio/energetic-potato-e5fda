9fec381c73d3d9e2764917f24ff2d4dc55bb1f71
Stackbit Code Editor <projects@stackbit.com>
2021-04-29 22:19:28 +0000

    service.js: changed

--- src/components/service.js
+++ src/components/service.js
@@ -5,6 +5,7 @@ import Img from 'gatsby-image';
 const Service = ({ service }) => (
     <div className="service service--left" data-sal="fade" data-sal-easing="ease-in-cubic" data-sal-duration="400">
         <div className="service__image-wrap">
+            <i className="item__icon material-icons text-primary">{ feature.icon }</i>
             <Img fluid={ service.image.fluid } alt="service item" className="service__image"/>
         </div>
         <h3 className="service__title">{ service.title }</h3>
