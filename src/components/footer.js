6ac7a1862896f872412e612dece0a658db7a4ec0
Stackbit Code Editor <projects@stackbit.com>
2021-05-24 19:18:35 +0000

    footer.js: changed

--- src/components/footer.js
+++ src/components/footer.js
@@ -87,7 +87,13 @@ const Footer = ({ menus }) => {
                     </ul>
                 </div> */}
                 
-                
+                <div>
+                        <!-- Calendly link widget begin -->
+<link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet">
+<script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async></script>
+<a href="" onclick="Calendly.initPopupWidget({url: 'https://calendly.com/launchbh'});return false;">Schedule time with me</a>
+<!-- Calendly link widget end -->
+                </div>
             </div>
         </footer>
     );
