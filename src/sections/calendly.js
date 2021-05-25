aff564c4ef3f1b7093df11de96abb0e6a7b68ffb
Pedro <pedro@reactbh.org>
2021-05-25 11:44:15 -0400

    use calendly only - remove form

--- src/sections/calendly.js
+++ src/sections/calendly.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
-import {InlineWidget} from 'react-calendly'
+import { InlineWidget } from 'react-calendly'
 
 class Calendly extends Component {
   render(){
     return (
-      <div>
         <InlineWidget url="https://calendly.com/launchbh" />
-      </div>
     );
   }
 }
