f7acb98890228074e76b498c06afb086924839fa
Pedro <pedro@reactbh.org>
2021-05-25 11:47:45 -0400

    refactor

--- src/sections/contact.js
+++ src/sections/contact.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Calendly from './calendly';
+import { InlineWidget } from 'react-calendly'
 
 const Contact = () => {
     return (
         <section id="contact" className="contact">
             <h2 className="section__title text-center mb-16" data-sal="fade" data-sal-easing="ease-in-cubic" style={{ paddingTop: "5rem", marginBottom: "0" }}>Contact</h2>
-            <Calendly />
+            <InlineWidget url="https://calendly.com/launchbh" />
         </section>   
     );
 };
