15cd9b8623e620c38787951d5c82f0d664faff08
Pedro <pedro@reactbh.org>
2021-05-30 15:54:41 -0400

    fit service images

--- src/components/service.js
+++ src/components/service.js
@@ -5,7 +5,7 @@ import Img from 'gatsby-image';
 const Service = ({ service }) => (
     <div className="service service--left" data-sal="fade" data-sal-easing="ease-in-cubic" data-sal-duration="400">
         <div className="service__image-wrap">
-            <Img fluid={ service.image.fluid } alt="service item" className="service__image"/>
+            <Img fluid={ service.image.fluid } alt="service item" className="service__image" imgStyle={{ objectFit: 'contain' }}/>
         </div>
         <h3 className="service__title">{ service.title }</h3>
         <p>{ service.description.description }</p>
